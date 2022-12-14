32f0720eb6f805ce7a73cbc2ecca7966803d65f7
JungwoongSong <song1932@gmail.com>
2022-12-14 10:33:42 +0900

    aa

--- class1.py
+++ class1.py
@@ -1,14 +1,18 @@
 # class1.py
 #클래스 정의
 class Person:
+    #클래스 맴버 변수(일종의 공통 데이터로 사용)
+    num_person = 0
     #초기화 메서드
     def __init__(self):
         self.name = "default name"
+        Person.num_person +=1
     #인스턴스 메서드
     def print(self):
         print("My name is {0}".format(self.name))
 
 #인스턴스 생성
 p1 = Person()
-#메서드 호출
-p1.print()
+p2 = Person()
+p3 = Person()
+print("인스턴스 갯수:{0}".format(Person.num_person))
